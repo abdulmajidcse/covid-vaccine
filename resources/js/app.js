7c88259cd88924c6d0fa9fc9ae4366981315e81c
Abdul Majid <abdulmajidcse@gmail.com>
2024-10-11 11:02:38 +0600

    added registration feature.

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,10 @@ import "./../../node_modules/flowbite-vue/dist/index.css";
 import { createApp } from "vue";
 import Root from "./Root.vue";
 import router from "./routes";
+import ToastPlugin from "vue-toast-notification";
+import 'vue-toast-notification/dist/theme-bootstrap.css';
 
 const app = createApp(Root);
 app.use(router);
+app.use(ToastPlugin);
 app.mount("#app");
